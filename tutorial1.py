65cba9754886fa09ddd3df056bbeed77fbafca98
kanchana109 <kanchananimbalkar@gmail.com>
2024-07-27 16:45:46 +0530

    Create tutorial1.py

---
+++ tutorial1.py
@@ -0,0 +1,52 @@
+#ctrl +/
+# for i in  "ritesh":
+#     print(i)
+
+
+### defining a fucntion
+def print_data():
+    print("Huballi -also called mini bombay")
+
+
+## call a fucntion
+print_data()
+
+
+
+
+fruits_list = [ "apple","mango","chikkoo"]
+
+print(fruits_list)
+
+print(type(fruits_list))
+
+
+fruits_list.append("watermelon")
+
+print(fruits_list)
+
+
+print(fruits_list[0:2])
+
+
+
+
+fruits_list.clear()
+
+
+print(fruits_list)
+
+
+
+
+####### dictionary
+
+dict1 = {
+    "name":"abc",   # name -key   and     "abc -value"
+    "place":"hubli",  #palce- key  and      value - hubali
+}
+print(dict1)
+
+print(dict1.keys())
+
+print(dict1.values())
